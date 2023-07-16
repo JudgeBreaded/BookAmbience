910c1e9bc22b8dcb162722afa18ebe8f77567360
Brett MacKinnon <brett.mackinnon9@gmail.com>
2023-07-16 16:23:34 -0400

    load book images'

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-
 document.getElementById('submitButton').addEventListener('click', (e) => {
-    e.preventDefault()
-    let bookShelf = document.getElementById('book-container')
-    let searchInput = document.getElementById('searchInput').value
+    e.preventDefault();
+    let bookShelf = document.getElementById('book-container');
+    let searchInput = document.getElementById('searchInput').value;
     fetch(`https://openlibrary.org/search.json?q=${searchInput}`)
         .then(res => res.json())
-        .then(data => { 
-            console.log(data)
+        .then(data => {
+            console.log(data);
+            const bookLoad = (data) => {
+                let bookData = data.docs.map(bookTitles => {
+                    return `<div class='col-4 mb-3'>
+                        <div class="card col-4 mb-3" style="width: 18rem;">
+                            <img class="card-img-top" src="https://covers.openlibrary.org/b/id/${bookTitles.cover_i}.jpg" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">${bookTitles.title}</h5>
+                                <p class="card-text">${bookTitles.first_publish_year}</p>
+                                <a href="#" class="btn btn-primary">Example</a>
+                            </div>
+                        </div>
+                    </div>`;
+                });
+                return bookData.join('');
+            };
+            bookShelf.innerHTML = bookLoad(data);
+        })
     })
-})
