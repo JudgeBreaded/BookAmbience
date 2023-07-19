f8b919dc4e2538d6ce085f2402766ebd03290a30
Brett MacKinnon <brett.mackinnon9@gmail.com>
2023-07-18 22:57:57 -0400

    connected Apple API to the Site

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.getElementById('submitButton').addEventListener('click', (e) => {
+document.getElementById('bookButton').addEventListener('click', (e) => {
     e.preventDefault();
     let bookShelf = document.getElementById('book-container');
-    let searchInput = document.getElementById('searchInput').value;
+    let searchInput = document.getElementById('searchInput1').value;
     fetch(`https://openlibrary.org/search.json?q=${searchInput}`)
         .then(results => results.json())
         .then(data => {
@@ -13,9 +13,9 @@ document.getElementById('submitButton').addEventListener('click', (e) => {
                         cover: `https://covers.openlibrary.org/b/id/${bookTitles.cover_i}.jpg`
                     };
                     localStorage.setItem(bookTitles.title, JSON.stringify(bookInfo))
-                    return `<div class='col-4 mb-3'>
-                        <div class="card" style="width: 18rem height: 18rem;">
-                            <img class="card-img-top" src="https://covers.openlibrary.org/b/id/${bookTitles.cover_i}.jpg" alt="Card image cap">
+                    return `<div class='col-2 mb-3'>
+                        <div class="card" style="width: 150px height: 150px;">
+                            <img class="card-img-top" src="https://covers.openlibrary.org/b/id/${bookTitles.cover_i}-M.jpg" alt="Card image cap">
                             <div class="card-body">
                                 <h5 class="card-title">${bookTitles.title}</h5>
                                 <p class="card-text">${bookTitles.first_publish_year}</p>
@@ -29,3 +29,35 @@ document.getElementById('submitButton').addEventListener('click', (e) => {
             bookShelf.innerHTML = bookLoad(data);
         })
     })
+
+    document.getElementById('musicButton').addEventListener('click', (e) => {
+        e.preventDefault();
+        let bookShelf = document.getElementById('book-container');
+        let searchInput = document.getElementById('searchInput1').value;
+        fetch(`https://itunes.apple.com/search?media=music&term=${searchInput}`)
+            .then(results => results.json())
+            .then(data => {
+                console.log(data)
+                const musicLoad = (data) => {
+                    let musicData = data.results.map(musicInfo => {
+                        let musicExtra = {
+                            title: musicInfo.trackName,
+                            cover: musicInfo.artworkUrl100
+                        };
+                        localStorage.setItem(musicInfo.trackName, JSON.stringify(musicExtra))
+                        return `<div class='col-2 mb-3'>
+                            <div class="card" style="width: 150px height: 150px;">
+                                <img class="card-img-top" src="${musicInfo.artworkUrl100}" alt="Card image cap">
+                                <div class="card-body">
+                                    <h5 class="card-title">${musicInfo.trackName}</h5>
+                                    <p class="card-text">${musicInfo.artistName}</p>
+                                    <a href="" class="btn btn-primary">More Info</a>
+                                </div>
+                            </div>
+                        </div>`;
+                    });
+                    return musicData.join('');
+                };
+                bookShelf.innerHTML = musicLoad(data);
+            })
+        })
