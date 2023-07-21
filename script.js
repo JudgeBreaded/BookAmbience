dc8d1d232d7abec383426649c6f6db0f497f6e64
Brett MacKinnon <brett.mackinnon9@gmail.com>
2023-07-20 21:17:09 -0400

    added random funcitonality

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ document.getElementById('bookButton').addEventListener('click', (e) => {
                         cover: `https://covers.openlibrary.org/b/id/${bookTitles.cover_i}.jpg`
                     };
                     localStorage.setItem(bookTitles.title, JSON.stringify(bookInfo))
-                    return `<div class='col-sm-6 col-md-4 col-lg-3 mb-3'>
-                        <div class="card" style="width: 150px height: 150px;">
+                    return `<div class='col-2 mb-3'>
+                        <div class="card text-center" style="width: 150px height: 150px;">
                             <img class="card-img-top" ${bookPicUrl}${bookTitles.cover_i}-L.jpg" alt="Card image cap">
                             <div class="card-body">
                                 <h5 class="card-title">${bookTitles.title}</h5>
@@ -50,8 +50,8 @@ document.getElementById('bookButton').addEventListener('click', (e) => {
                             cover: musicInfo.artworkUrl100
                         };
                         localStorage.setItem(musicInfo.trackName, JSON.stringify(musicExtra))
-                        return `<div class='col-sm-6 col-md-4 col-lg-3 mb-3'>
-                            <div class="card" style="width: 150px height: 150px;">
+                        return `<div class='col-2 mb-3'>
+                            <div class="card text-center" style="width: 150px height: 150px;">
                                 <img class="card-img-top" src="${musicInfo.artworkUrl100}" alt="Card image cap">
                                 <div class="card-body">
                                     <h5 class="card-title">${musicInfo.trackName}</h5>
@@ -66,3 +66,85 @@ document.getElementById('bookButton').addEventListener('click', (e) => {
                 bookShelf.innerHTML = musicLoad(data);
             })
         })
+
+        document.getElementById('musicRandom').addEventListener('click', (e) => {
+            e.preventDefault();
+            let bookShelf = document.getElementById('book-container');
+            const genreGenerator = () =>{
+            let genreList = ["Forest Escape", 'Ocean Landscape','Fable ost', 'medieval ambience','desert ambience', 'dungeon ambience', 'battle ambience', 'celtic ambience', 'how to train your dragon', 'scotland ambience', 'Space Ambience', 'cosmic horror']
+            console.log(genreGenerator)
+            let i = Math.floor(Math.random() * 11)
+            console.log(i)
+            return genreList[i]
+            }
+            //let searchInput = document.getElementById('searchInput1').value;
+            fetch(`https://itunes.apple.com/search?media=music&attribute&term=${genreGenerator()}`)
+                .then(results => results.json())
+                .then(data => {
+                    console.log(data)
+                    const musicLoad = (data) => {
+                        let musicData = data.results.map(musicInfo => {
+                            let musicExtra = {
+                                title: musicInfo.trackName,
+                                cover: musicInfo.artworkUrl100
+                            };
+                            localStorage.setItem(musicInfo.trackName, JSON.stringify(musicExtra))
+                            return `<div class='col-2 mb-3'>
+                                <div class="card text-center" style="width: 150px height: 150px;">
+                                    <img class="card-img-top" src="${musicInfo.artworkUrl100}" alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${musicInfo.trackName}</h5>
+                                        <p class="card-text">${musicInfo.artistName}</p>
+                                        <a href="${musicInfo.previewUrl}" class="btn btn-primary">Preview</a>
+                                    </div>
+                                </div>
+                            </div>`;
+                        });
+                        return musicData.join('');
+                    };
+                    bookShelf.innerHTML = musicLoad(data);
+                })
+            })
+
+            document.getElementById('bookRandom').addEventListener('click', (e) => {
+                e.preventDefault();
+                let bookShelf = document.getElementById('book-container');
+                const genreGenerator = () =>{
+                let genreList = ["Forest Escape", 'Ocean Landscape','Fable ost', 'medieval ambience','desert ambience', 'dungeon ambience', 'battle ambience', 'celtic ambience', 'how to train your dragon', 'scotland ambience', 'Space Ambience', 'cosmic horror']
+                console.log(genreGenerator)
+                let i = Math.floor(Math.random() * 11)
+                console.log(i)
+                return genreList[i]
+                }
+                //let searchInput = document.getElementById('searchInput1').value;
+                fetch(`https://itunes.apple.com/search?media=music&attribute&term=${genreGenerator()}`)
+                    .then(results => results.json())
+                    .then(data => {
+                        console.log(data)
+                        const musicLoad = (data) => {
+                            let musicData = data.results.map(musicInfo => {
+                                let musicExtra = {
+                                    title: musicInfo.trackName,
+                                    cover: musicInfo.artworkUrl100
+                                };
+                                localStorage.setItem(musicInfo.trackName, JSON.stringify(musicExtra))
+                                return `<div class='col-2 mb-3'>
+                                    <div class="card text-center" style="width: 150px height: 150px;">
+                                        <img class="card-img-top" src="${musicInfo.artworkUrl100}" alt="Card image cap">
+                                        <div class="card-body">
+                                            <h5 class="card-title">${musicInfo.trackName}</h5>
+                                            <p class="card-text">${musicInfo.artistName}</p>
+                                            <audio controls volume>
+                                            <source src="${musicInfo.previewUrl}" volume=0.2">
+                                            </audio>
+                                            <a href="${musicInfo.previewUrl}" class="btn btn-primary">Preview</a>
+                                        </div>
+                                    </div>
+                                </div>`;
+                            });
+                            return musicData.join('');
+                        };
+                        bookShelf.innerHTML = musicLoad(data);
+                    })
+                })
+    
